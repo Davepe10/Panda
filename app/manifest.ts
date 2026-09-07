0e5f95f0f443c54a68940697259c56b0896e81d0
Javier Alexander <javieralexanderpe@gmail.com>
2026-09-06 19:04:21 -0500

    Delete app directory

--- app/manifest.ts
+++
@@ -1,2 +0,0 @@
-import type {MetadataRoute} from 'next'
-export default function manifest():MetadataRoute.Manifest{return{name:'Nosotros',short_name:'Nosotros',description:'Un espacio privado para dos.',start_url:'/app',display:'standalone',background_color:'#f8f5f1',theme_color:'#332e35'}}
